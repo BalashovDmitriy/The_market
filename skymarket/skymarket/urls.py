9039ce100e84d84dcc1d4fd98535a3c86da47480
dmitriy.balashov <reaver_std@mail.ru>
2023-12-12 12:31:24 +0500

    fixes

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -3,8 +3,6 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-# TODO здесь необходимо подклюючит нужные нам urls к проекту
-
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
