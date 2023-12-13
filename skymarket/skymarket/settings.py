c7c3bbe442fa860c20358a4348e9d9878f9e84cb
dmitriy.balashov <reaver_std@mail.ru>
2023-12-13 10:26:07 +0500

    fix

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -84,8 +84,7 @@ WSGI_APPLICATION = "skymarket.wsgi.application"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination"
+    )
 }
 # Здесь мы настраиваем Djoser
 DJOSER = {
