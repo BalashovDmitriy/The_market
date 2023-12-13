c783e3233ec401690cee0fc26f4cae4282e1737e
dmitriy.balashov <reaver_std@mail.ru>
2023-12-13 08:23:21 +0500

    add swagger

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -2,9 +2,26 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="The_market API",
+      default_version='v1',
+      description="The_market SPA backend API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="riv74x@gmail.com"),
+      license=openapi.License(name="Free Software License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
