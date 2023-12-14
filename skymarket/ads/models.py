0adffc00e338c0681735bf2faaed3bcc0ed11357
dmitriy.balashov <reaver_std@mail.ru>
2023-12-14 11:23:18 +0500

    add ads and comment models

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -3,10 +3,31 @@ from django.db import models
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=100, verbose_name="Название товара")
+    price = models.PositiveIntegerField(verbose_name="Цена товара")
+    description = models.TextField(verbose_name="Описание товара")
+    image = models.ImageField(verbose_name="Изображение товара")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Продавец")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ["-created_at"]
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.TextField(verbose_name="Текст отзыва")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор отзыва")
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, verbose_name="Товар отзыва")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
