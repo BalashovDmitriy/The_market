681555c5572a032ba082767eff4ab7f54470b212
dmitriy.balashov <reaver_std@mail.ru>
2023-12-14 11:33:23 +0500

    add endpoints for ads and comments

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,15 +1,19 @@
 from rest_framework import pagination, viewsets
 
+from skymarket.ads.models import Ad, Comment
+from skymarket.ads.serializers import AdSerializer, CommentSerializer
+
 
 class AdPagination(pagination.PageNumberPagination):
-    pass
+    page_size = 4
 
 
-# TODO view функции. Предлагаем Вам следующую структуру - но Вы всегда можете использовать свою
 class AdViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    pagination_class = AdPagination
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
-
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
