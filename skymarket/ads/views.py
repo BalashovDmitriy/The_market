2c70cbabeb717d764f2c00b92e76dc9824ff0447
dmitriy.balashov <reaver_std@mail.ru>
2023-12-15 09:39:10 +0500

    add filters

--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,7 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import pagination, viewsets
 
 from ads.models import Ad, Comment
 from ads.serializers import AdSerializer, CommentSerializer, AdDetailSerializer
+
+from ads.filters import AdFilter
 
 
 class AdPagination(pagination.PageNumberPagination):
@@ -11,6 +14,8 @@ class AdPagination(pagination.PageNumberPagination):
 class AdViewSet(viewsets.ModelViewSet):
     queryset = Ad.objects.all()
     pagination_class = AdPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdFilter
 
     def get_serializer_class(self):
         if self.action == "list":
