732a20cff13e17901ab77bda04ab2ca90bcf738a
dmitriy.balashov <reaver_std@mail.ru>
2023-12-17 21:19:07 +0500

    add serializers to comments

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -4,9 +4,29 @@ from ads.models import Comment, Ad
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author_first_name = serializers.CharField(source="author.first_name")
+    author_last_name = serializers.CharField(source="author.last_name")
+    author_id = serializers.CharField(source="author.id")
+    author_image = serializers.CharField(source="author.image")
+
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = (
+            "pk",
+            "text",
+            "author_id",
+            "created_at",
+            "author_first_name",
+            "author_last_name",
+            "ad_id",
+            "author_image",
+        )
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("text",)
 
 
 class AdSerializer(serializers.ModelSerializer):
